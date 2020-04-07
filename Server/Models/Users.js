44a1ebf93060b5a845006c8acef20c62f548f727
Haonan Quan <quan.h@husky.neu.edu>
2020-04-07 18:36:52 -0400

    user model

--- Server/Models/Users.js
+++ Server/Models/Users.js
@@ -1,7 +1,8 @@
 'use strict';
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
+const Schema = mongoose.Schema,
+                jwt = require('jsonwebtoken'),
+                bcrypt = require('bcrypt');
 
 /**
  * Mongoose schema for Users object.
@@ -34,6 +35,69 @@ const Schema = mongoose.Schema;
      },
      profileImage:{
         type:String,
-    }
+    },
+    //The videos this user have
+    videos:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Video'
+        }
+    ],
+    // The videos this user like
+    liked:[
+        {
+        type:Schema.Types.ObjectId,
+        ref:'Video'
+        }
+    ],
+    // The user this user subscribe
+    subscribe:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'User'
+        }
+    ],
+    // The video this user added to favorite
+    favorite:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Video'
+        }
+    ],
+    // The fans of this user
+    subscribed:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'User'
+        }
+    ]
+ },{
+    versionKey: false
+  }
+ );
+ // Duplicate the id field as mongoose returns _id field instead of id.
+memberSchema.virtual('id').get(function(){
+    return this._id.toHexString();
+  });
+  // Ensure virtual fields are serialised.
+  memberSchema.set('toJSON', {
+    virtuals: true
+  });
+// decode the password in database and compare to the input password
 
- });
+  UserSchema.methods.verifyPassword=function(input_password){
+    return bcrypt.compareSync(to_check_password, this.password);
+  }
+/**
+ * this jwt is going to help user no need to login again
+ *
+ * @returns a token file used to verify the identity 
+ */
+memberSchema.methods.generateJwt = function () {
+    return jwt.sign({ _id: this._id },
+      "SECRET#123",
+    {
+        expiresIn: "1day",
+    }
+    );
+  }
