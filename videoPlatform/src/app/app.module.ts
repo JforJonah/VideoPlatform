2d02a57591077fb35610d373331a7c0d985ed019
Hongji Luo <luo.ho@husky.neu.edu>
2020-04-07 15:22:20 -0400

    init front-end part

--- videoPlatform/src/app/app.module.ts
+++ videoPlatform/src/app/app.module.ts
@@ -6,10 +6,26 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NbThemeModule, NbLayoutModule } from '@nebular/theme';
 import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { AuthComponent } from './auth/auth.component';
+import { NavBarComponent } from './pages/nav-bar/nav-bar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { VideodetailComponent } from './pages/videodetail/videodetail.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { FavoriteComponent } from './pages/favorite/favorite.component';
+import { WatchlaterComponent } from './pages/watchlater/watchlater.component';
+import { HistoryComponent } from './pages/history/history.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    NavBarComponent,
+    HomeComponent,
+    VideodetailComponent,
+    ProfileComponent,
+    FavoriteComponent,
+    WatchlaterComponent,
+    HistoryComponent
   ],
   imports: [
     BrowserModule,
