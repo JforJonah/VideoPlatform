30e1bc082cc7a77f4199ac81dca809c952049b90
Lu Liu <liu.lu7@husky.neu.edu>
2020-04-11 16:27:37 -0400

    profile component ts and part of html

--- videoPlatform/src/app/pages/profile/profile.component.ts
+++ videoPlatform/src/app/pages/profile/profile.component.ts
@@ -7,9 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
+  titleArr =['My Video', 'Liked Videos', 'Subscirptions', 'History'];
+  title: string;
+  Arr = [1, 2, 3, 4];
+  Arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+  pageStatus ='proPage';
   constructor() { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  more(title){
+    console.log(title);
+    this.pageStatus = 'morePage';
+    this.title = title;
+  }
+  return(){
+    this.pageStatus = 'proPage';
+  }
 }
