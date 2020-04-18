bed33cbc29fbe92802df235dfa5e6bcabb00db40
Haonan Quan <quan.h@husky.neu.edu>
2020-04-17 20:54:57 -0400

    correction

--- Server/Controllers/UserController.js
+++ Server/Controllers/UserController.js
@@ -93,7 +93,7 @@ exports.signUp=function(request,response){
  * @param {*} next
  */
 exports.login=function(request,response,next){
-    console.log(request.body);
+    // console.log(request.body);
     passport.authenticate('local',function(err, user, info) {
         // 如果这个函数被调用了，说明认证成功。
         // `req.user` 包含已认证的用户
@@ -135,7 +135,7 @@ exports.userInfo=(req,res)=>{
  */
 exports.userUpdate=(req,res)=>{
     let updateUser=req.body;
-    let id=req.id;
+    let id=req.params.id;
     const resolve=(user)=>{
         res.status(200);
         res.json({"token":user.generateJwt(),"msg":'ok'});
