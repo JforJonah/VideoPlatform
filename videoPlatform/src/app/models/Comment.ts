7daa20ef1882b81672803a06afc8c58fb4483dd2
Wang Bochao <wang.boch@husky.neu.edu>
2020-04-18 17:35:56 -0400

    update comment

---
+++ videoPlatform/src/app/models/Comment.ts
@@ -0,0 +1,7 @@
+export class Comment{
+    id : String;
+    txt : String;
+    videoId: String;
+    createDate: Date;
+    auth:String;
+}
--- videoPlatform/src/app/pages/videodetail/videodetail.component.ts
+++ videoPlatform/src/app/pages/videodetail/videodetail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import {NbMenuItem, NbMenuService ,NbCardModule} from '@nebular/theme';
 import { ChangeDetectionStrategy } from '@angular/core';
+import {Video} from '../../models/Video';
+import {User} from '../../models/User';
+import {Comment} from '../../models/Comment';
+import {UserService} from '../../server/user.service';
+import {VideoService} from '../../server/video.service';
 
 @Component({
   selector: 'app-videodetail',
@@ -12,7 +17,11 @@ export class VideodetailComponent implements OnInit {
 
   
 
-  constructor() { }
+  constructor(private videoService: VideoService,
+              private userService: UserService) {
+                
+
+               }
 
   ngOnInit(): void {
   }
