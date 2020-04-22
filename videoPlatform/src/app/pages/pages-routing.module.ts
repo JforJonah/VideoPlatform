22edd7dd54ef12a99532c090e30f90c5abe45ace
Hongji Luo <luo.ho@husky.neu.edu>
2020-04-22 01:46:06 -0400

    update editvideo page, fix minor bugs, update nav-bar

--- videoPlatform/src/app/pages/pages-routing.module.ts
+++ videoPlatform/src/app/pages/pages-routing.module.ts
@@ -4,12 +4,11 @@ import {HomeComponent} from './home/home.component';
 import {FavoriteComponent} from './favorite/favorite.component';
 import {UploadvideoComponent} from './uploadvideo/uploadvideo.component';
 import {HistoryComponent} from './history/history.component';
-import {WatchlaterComponent} from './watchlater/watchlater.component';
 import {FollowsComponent} from './follows/follows.component';
 import {ProfileComponent} from './profile/profile.component';
 import {NavBarComponent} from './nav-bar/nav-bar.component';
 import {OtherprofileComponent} from './otherprofile/otherprofile.component';
-import {VideodetailComponent} from '../pages/videodetail/videodetail.component';
+import {VideodetailComponent} from './videodetail/videodetail.component';
 import {EditvideoComponent} from './profile/editvideo/editvideo.component';
 
 
@@ -22,12 +21,11 @@ const routes: Routes = [
       {path: 'favorite', component: FavoriteComponent},
       {path: 'upload', component: UploadvideoComponent},
       {path: 'history', component: HistoryComponent},
-      {path: 'watchlater', component: WatchlaterComponent},
       {path: 'follows', component: FollowsComponent},
       {path: 'profile', component: ProfileComponent},
       {path: 'details/:id', component: VideodetailComponent},
       {path: 'profile/:id', component: OtherprofileComponent},
-      {path: 'profile/:videoid', component: EditvideoComponent},
+      {path: 'updatevideo/:id', component: EditvideoComponent}
     ]
   }
   ,
